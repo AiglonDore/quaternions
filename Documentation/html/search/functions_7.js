782e94e12f37e9ad6a41cc8238a685f5b2214bc4
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-11-25 11:09:16 +0100

    Updated doc

---
+++ Documentation/html/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_7equaternion_51',['~Quaternion',['../classensiie_1_1_quaternion.html#af7028b25bd9858eb217791bf8d0d476c',1,'ensiie::Quaternion::~Quaternion()'],['../classensiie_1_1_quaternion.html#af7028b25bd9858eb217791bf8d0d476c',1,'ensiie::Quaternion::~Quaternion()']]]
+];
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Quaternions
 
 A C++ class that handles quaternions, made with `double`.
+A template class exists for other types, but it won't be as accurate.
+Only the `double` model will be documented and tested.
