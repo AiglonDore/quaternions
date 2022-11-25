a50cbc6d59df97be4f8c12f6ba46f5f0244c2fb4
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-11-25 11:02:00 +0100

    Template class built
    Signed-off-by: Thomas Roiseux <thomas.roiseux@outlook.com>

--- template/quaternion_template.cpp
+++ template/quaternion_template.cpp
@@ -12,32 +12,39 @@
 #include "quaternion_template.h"
 #include <exception>
 
-ensiie::Quaternion::Quaternion() : u(0), v(0), w(0), t(0)
+template<class T>
+ensiie::Quaternion<T>::Quaternion() : u(0), v(0), w(0), t(0)
 {
 }
 
-ensiie::Quaternion::Quaternion(double x) : t(x), u(0), v(0), w(0)
+template<class T>
+ensiie::Quaternion<T>::Quaternion(T x) : t(x), u(0), v(0), w(0)
 {
 }
 
-ensiie::Quaternion::Quaternion(double x, double y) : t(x), u(y), v(0), w(0)
+template<class T>
+ensiie::Quaternion<T>::Quaternion(T x, T y) : t(x), u(y), v(0), w(0)
 {
 }
 
-ensiie::Quaternion::Quaternion(double x, double y, double z, double w) : t(x), u(y), v(z), w(w)
+template<class T>
+ensiie::Quaternion<T>::Quaternion(T x, T y, T z, T w) : t(x), u(y), v(z), w(w)
 {
 }
 
-ensiie::Quaternion::~Quaternion()
+template<class T>
+ensiie::Quaternion<T>::~Quaternion()
 {
 }
 
-double ensiie::Quaternion::norm() const 
+template<class T>
+T ensiie::Quaternion<T>::norm() const 
 {
     return std::sqrt(t * t + u * u + v * v + w * w);
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator+=(const Quaternion& q)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator+=(const Quaternion& q)
 {
     t += q.t;
     u += q.u;
@@ -46,7 +53,8 @@ ensiie::Quaternion& ensiie::Quaternion::operator+=(const Quaternion& q)
     return *this;
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator-=(const Quaternion& q)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator-=(const Quaternion& q)
 {
     t -= q.t;
     u -= q.u;
@@ -55,12 +63,13 @@ ensiie::Quaternion& ensiie::Quaternion::operator-=(const Quaternion& q)
     return *this;
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator*=(const Quaternion& q)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator*=(const Quaternion& q)
 {
-    double t1 = t;
-    double u1 = u;
-    double v1 = v;
-    double w1 = w;
+    T t1 = t;
+    T u1 = u;
+    T v1 = v;
+    T w1 = w;
     t = t1 * q.t - u1 * q.u - v1 * q.v - w1 * q.w;
     u = t1 * q.u + u1 * q.t + v1 * q.w - w1 * q.v;
     v = t1 * q.v - u1 * q.w + v1 * q.t + w1 * q.u;
@@ -68,16 +77,17 @@ ensiie::Quaternion& ensiie::Quaternion::operator*=(const Quaternion& q)
     return *this;
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator/=(const Quaternion& q)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator/=(const Quaternion& q)
 {
     if (q.norm() <= 1e-15)
     {
         throw std::invalid_argument("Division by zero");
     }
-    double t1 = t;
-    double u1 = u;
-    double v1 = v;
-    double w1 = w;
+    T t1 = t;
+    T u1 = u;
+    T v1 = v;
+    T w1 = w;
     t = (t1 * q.t + u1 * q.u + v1 * q.v + w1 * q.w) / (q.t * q.t + q.u * q.u + q.v * q.v + q.w * q.w);
     u = (u1 * q.t - t1 * q.u - w1 * q.v + v1 * q.w) / (q.t * q.t + q.u * q.u + q.v * q.v + q.w * q.w);
     v = (v1 * q.t + w1 * q.u - t1 * q.v - u1 * q.w) / (q.t * q.t + q.u * q.u + q.v * q.v + q.w * q.w);
@@ -85,7 +95,8 @@ ensiie::Quaternion& ensiie::Quaternion::operator/=(const Quaternion& q)
     return *this;
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator*=(double x)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator*=(T x)
 {
     t *= x;
     u *= x;
@@ -94,7 +105,8 @@ ensiie::Quaternion& ensiie::Quaternion::operator*=(double x)
     return *this;
 }
 
-ensiie::Quaternion& ensiie::Quaternion::operator/=(double x)
+template<class T>
+ensiie::Quaternion<T>& ensiie::Quaternion<T>::operator/=(T x)
 {
     if (x <= 1e-15)
     {
@@ -107,57 +119,65 @@ ensiie::Quaternion& ensiie::Quaternion::operator/=(double x)
     return *this;
 }
 
-std::ostream& ensiie::operator<<(std::ostream& os, const Quaternion& q)
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const ensiie::Quaternion<T>& q)
 {
     os << q.t << " + " << q.u << "i + " << q.v << "j + " << q.w << "k";
     return os;
 }
 
-ensiie::Quaternion ensiie::operator+(const Quaternion& q1, const Quaternion& q2)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator+(const Quaternion<T>& q1, const Quaternion<T>& q2)
 {
-    ensiie::Quaternion copy(q1);
+    ensiie::Quaternion<T> copy(q1);
     copy += q2;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator-(const Quaternion& q1, const Quaternion& q2)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator-(const Quaternion<T>& q1, const Quaternion<T>& q2)
 {
-    ensiie::Quaternion copy(q1);
+    ensiie::Quaternion<T> copy(q1);
     copy -= q2;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator*(const Quaternion& q1, const Quaternion& q2)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator*(const Quaternion<T>& q1, const Quaternion<T>& q2)
 {
-    ensiie::Quaternion copy(q1);
+    ensiie::Quaternion<T> copy(q1);
     copy *= q2;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator/(const Quaternion& q1, const Quaternion& q2)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator/(const Quaternion<T>& q1, const Quaternion<T>& q2)
 {
-    ensiie::Quaternion copy(q1);
+    ensiie::Quaternion<T> copy(q1);
     copy /= q2;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator*(const Quaternion& q, double x)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator*(const Quaternion<T>& q, T x)
 {
-    ensiie::Quaternion copy(q);
+    ensiie::Quaternion<T> copy(q);
     copy *= x;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator*(double x, const Quaternion& q)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator*(T x, const Quaternion<T>& q)
 {
-    ensiie::Quaternion copy(q);
+    ensiie::Quaternion<T> copy(q);
     copy *= x;
     return copy;
 }
 
-ensiie::Quaternion ensiie::operator/(const Quaternion& q, double x)
+template<typename T>
+ensiie::Quaternion<T> ensiie::operator/(const Quaternion<T>& q, T x)
 {
-    ensiie::Quaternion copy(q);
+    ensiie::Quaternion<T> copy(q);
     copy /= x;
     return copy;
 }
